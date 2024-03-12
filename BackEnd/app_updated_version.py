80228a4bae5f86da46fd4d2121bbdcdb94719132
umamaheswar <40281735+Skyrider3@users.noreply.github.com>
2024-03-11 21:01:37 -0400

    Updated biomarkers_reference_values.py file with biomarker reference values

--- BackEnd/app_updated_version.py
+++ BackEnd/app_updated_version.py
@@ -1,4 +1,4 @@
-import os
+import os, json
 from fastapi.responses import JSONResponse, StreamingResponse
 from fastapi.security import OAuth2PasswordBearer
 from fastapi.middleware.cors import CORSMiddleware
@@ -17,7 +17,7 @@ from passlib.hash import bcrypt  # Install passlib with: pip install passlib
 import fastapi
 # from database import SessionLocal, ExcelFile
 from typing import Optional, Dict, Any
-from biomarker_reference_values import get_reference_values, get_reference_values_barplot
+from biomarker_reference_values import get_reference_values, get_reference_values_barplot, biomarker_LowHigh_range_values
 
 
 app = FastAPI()
@@ -102,138 +102,6 @@ class ExcelDataRow(BaseModel):
 # Define a Pydantic model for the overall response
 class ExcelDataResponse(BaseModel):
     data: list[ExcelDataRow]
-
-# # @app.get("/get_excel_data/{file_id}")
-# @app.get("/get_excel_data/{file_id}")
-# async def get_excel_data(file_id: int):
-#     db = SessionLocal()
-
-#     try:
-#         # Retrieve file data from the database
-#         excel_file = db.query(ExcelFile).filter(ExcelFile.id == file_id).first()
-#         if not excel_file:
-#             raise HTTPException(status_code=404, detail="File not found")
-
-#         # Parse Excel file data as bytes
-#         content_bytes = excel_file.file_data
-
-#         # Use BytesIO to create a file-like object
-#         excel_data = BytesIO(content_bytes)
-
-#         # Process the Excel data using pandas
-#         df = pd.read_excel(excel_data)  # Use read_excel for Excel files
-#         # print(df)
-
-#         # Assuming df is your DataFrame
-#         # # Check for missing fields (columns)
-#         # missing_fields = df.columns[df.isnull().any()].tolist()
-
-#         # if missing_fields:
-#         #     print("DataFrame has missing fields:", missing_fields)
-#         # else:
-#         #     print("DataFrame does not have any missing fields.")
-        
-#         ##to check column data types
-#         # column_types = df.dtypes
-#         # print(column_types)
-
-#         # Convert DataFrame to JSON
-#         #json_content = df.to_dict(orient="records", default_handler=str)  # Use default_handler to handle non-serializable values
-#         json_content = json.loads(df.to_json(orient="records"))
-#         # print(json_content)
-
-
-#         # # process the Nan values **update this code snippet for handling all null or Nan Values**
-#         # for row in json_content:
-#         #     row["Low Range"] = round(pd.to_numeric(row["Low Range"], errors="coerce"),2)
-#         #     row["High Range"] = round(pd.to_numeric(row["High Range"], errors="coerce"),2)
-#         #     #row["UOM"] = pd.to_numeric(row["UOM"], errors="coerce") # the metrics are not a numeric ones
-
-#         # Process json content  as needed and send data to frontend
-#         response_model = ExcelDataResponse(
-#             data=[
-#                 ExcelDataRow(
-#                     **{col: row[col] for col in df.columns} #if you have columns with no date-time formatt
-#                     #**{col: row[col].strftime('%Y-%m-%d') if isinstance(row[col], datetime) else row[col] for col in df.columns} #to handle date time formatt
-#                 )
-#                 for row in json_content
-#             ]
-#         )   
-
-
-#         # Further process the data to the desired format
-#         formatted_data = []
-#         for item in response_model.data:
-#             # Construct the formatted item including additional columns
-#             formatted_item = {
-#                 "Category": item.Category,
-#                 "Biomarker": item.Biomarker,
-#                 "UOM": item.UOM,
-#                 "Low_Range": item.Low_Range,
-#                 "High_Range": item.High_Range,
-#             }
-            
-#             # Extract additional columns dynamically
-#             additional_columns = {key: getattr(item, key) for key in item.dict().keys() if key not in ["category", "biomarker", "uom", "low_range", "high_range"]}
-            
-#             # Update the formatted item with additional columns
-#             formatted_item.update(additional_columns)
-            
-#             formatted_data.append(formatted_item)
-
-#         return formatted_data
-
-
-#         # # # For now, returning the data as JSON
-#         # return JSONResponse(content=json_content, status_code=200)
-
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
-
-#     finally:
-#         db.close()
-
-
-
-# @app.get("/get_excel_data/{file_id}")
-# async def get_excel_data(file_id: int):
-#     db = SessionLocal()
-
-#     try:
-#         # Retrieve file data from the database
-#         excel_file = db.query(ExcelFile).filter(ExcelFile.id == file_id).first()
-#         if not excel_file:
-#             raise HTTPException(status_code=404, detail="File not found")
-
-#         # Parse Excel file data as bytes
-#         content_bytes = excel_file.file_data
-#         excel_data = BytesIO(content_bytes)
-
-#         # Process the Excel data using pandas
-#         df = pd.read_excel(excel_data)
-
-#         # Convert DataFrame to JSON
-#         json_content = json.loads(df.to_json(orient="records"))
-
-#         # Process json content and send data to frontend
-#         response_data = []
-#         for row in json_content:
-#             response_dict = {
-#                 "name": row["Biomarker"],
-#                 "value": row["22-7-2022"],  # Example value, replace with desired value ##### make this value passed from endpoint so that you can choose the date from UI
-#                 "units": row["UOM"],
-#                 "first": False,
-#                 "referenceValues": get_reference_values(row["Biomarker"])  # Get reference values dynamically
-#             }
-#             response_data.append(response_dict)
-
-#         return response_data
-
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
-
-#     finally:
-#         db.close()
 
 
 
@@ -329,69 +197,7 @@ async def get_biomarkers(file_id: int) :
 
 
 
-##page3 dynamic part of it 
-# Sample data for reference values (you can replace this with your actual data) --------------fill this 
 
-reference_values = {
-    "Ferritin": {"normal_range": [11, 306], "units": "ng/mL"},
-    "HCT": {"normal_range": [36, 46], "units": "g/dL"},
-    "Hematocrit": {"normal_range": [34, 46.6], "units": "g/dL"},
-    "Iron Serum": {"normal_range": [28, 170], "units": "ug/dL"},
-    "Mean Corp HGB": {"normal_range": [26.6, 33], "units": "pg"},
-    "Mean Corp HGB Conc.": {"normal_range": [31.5, 35.7], "units": "g/dL"},
-    "Mean Corp Volume": {"normal_range": [79, 97], "units": "fL"},
-    "Mean Corpuscular Volume (MCV)": {"normal_range": [80, 100], "units": "fL"},
-    "Mean Platelet Volume (MPV)": {"normal_range": [8.4, 12], "units": "fl"},
-    "Platelet": {"normal_range": [320, 320], "units": "x10e3/uL"},
-    "RBC": {"normal_range": [4, 5.2], "units": "M/uL"},
-    "RED DISTRIB. WIDTH": {"normal_range": [12.3, 15.4], "units": "g/dL"},
-    "Cholesterol": {"normal_range": [100, 200], "units": "mg/dL"},
-    "HDL Cholesterol": {"normal_range": [35, 100], "units": "mg/dL"},
-    "LDL Cholesterol": {"normal_range": [50, 129], "units": "mg/dL"},
-    "Triglycerides": {"normal_range": [40, 150], "units": "mg/dL"},
-    "Basophil #": {"normal_range": [44.4, 44.4], "units": "x10e3/uL"},
-    "Basophil %": {"normal_range": [1, 1], "units": "g/dL"},
-    "Eosinophil #": {"normal_range": [0.3, 0.3], "units": "x10e3/uL"},
-    "Eosinophil %": {"normal_range": [4, 4], "units": "g/dL"},
-    "Lymphocyte #": {"normal_range": [2, 2], "units": "x10e3/uL"},
-    "Lymphocyte %": {"normal_range": [24.6, 24.6], "units": "g/dL"},
-    "Monocyte #": {"normal_range": [0.6, 0.6], "units": "x10e3/uL"},
-    "Monocyte %": {"normal_range": [8.2, 8.2], "units": "g/dL"},
-    "Neutrophil #": {"normal_range": [4.8, 4.8], "units": "x10e3/uL"},
-    "Neutrophil %": {"normal_range": [61, 61], "units": "g/dL"},
-    "WBC": {"normal_range": [4.5, 11], "units": "k/uL"},
-    "Immature Granulocytes (Auto) #": {"normal_range": [0.03, 0.03], "units": "x10e3/uL"},
-    "Immature Granulocytes (Auto) %": {"normal_range": [0.4, 0.4], "units": "g/dL"},
-    "ALT": {"normal_range": [7, 33], "units": "U/L"},
-    "ALT (SGPT)": {"normal_range": [44.4, 40], "units": "IU/L"},
-    "Glucose": {"normal_range": [70, 110], "units": "mg/dL"},
-    "Hemoglobin A1C": {"normal_range": [4.3, 5.9], "units": "g/dL"},
-    "Albumin": {"normal_range": [4, 5], "units": "g/dL"},
-    "Albumin, BLD": {"normal_range": [3.5, 5.5], "units": "g/dL"},
-    "AST": {"normal_range": [9, 32], "units": "U/L"},
-    "AST (SGOT)": {"normal_range": [44.4, 32], "units": "IU/L"},
-    "Potassium": {"normal_range": [3.4, 5], "units": "mmol/L"},
-    "Sodium": {"normal_range": [134, 144], "units": "mmol/L"},
-    "A/G Ratio": {"normal_range": [1.2, 2.2], "units": "g/dL"},
-    "ABSOLUTE NRBC": {"normal_range": [44.4, 0.01], "units": "k/uL"},
-    "Alanine Aminotransferase": {"normal_range": [14, 54], "units": "U/L"},
-    "Albumin / Globulin Ratio": {"normal_range": [1, 2.6], "units": "g/dL"},
-    "Alkaline Phosphatase": {"normal_range": [39, 117], "units": "IU/L"},
-    "Anion Gap": {"normal_range": [3, 17], "units": "mmol/L"},
-    "Aspartate Amino Transferase": {"normal_range": [15, 41], "units": "U/L"},
-    "Bilirubin total": {"normal_range": [44.4, 1.2], "units": "mg/dL"},
-    "BUN": {"normal_range": [8, 25], "units": "mg/dL"},
-    "Bun / CREAT": {"normal_range": [9, 23], "units": "g/dL"},
-    "Calcium": {"normal_range": [8.7, 10.2], "units": "mg/dL"},
-    "Carbon Dioxide, Total": {"normal_range": [20, 29], "units": "mmol/L"},
-    "Cardiac Risk Ratio": {"normal_range": [44.4, 5], "units": "g/dL"},
-    "Chloride": {"normal_range": [96, 106], "units": "mmol/L"},
-    "Cholesterol / HDL Ratio": {"normal_range": [3.27, 7.05], "units": "g/dL"},
-    "Creatinine": {"normal_range": [0.57, 1], "units": "mg/dL"},
-    "EGFR": {"normal_range": [59, 120], "units": "mL/min/1.73m2"},
-   
-}
- #---this is connected to get_biomarkers_info() function
 
 ##page3 dynamic part of it 
 # Sample data for reference values (you can replace this with your actual data) --------------fill this
@@ -417,7 +223,8 @@ async def get_biomarker_info(file_id: int, biomarker_name: str):
 
         # Extract the value of the biomarker from the "22-7-2022" column
         biomarker_value = biomarker_row["22-7-2022"].values[0]  # Assuming there is only one value for the biomarker ------------> need to check
-
+        
+        reference_values = biomarker_LowHigh_range_values()
 
         if biomarker_name not in reference_values:
             raise HTTPException(status_code=404, detail="Biomarker not found")
@@ -504,75 +311,6 @@ openai.api_key = OPENAI_API_KEY
 class HealthRecommendations(BaseModel):
     recommendations: dict
 
-
-
-# @app.get("/generate_text/{file_id}/fooddietprecautions", response_model=HealthRecommendations)
-# async def generate_text_food_diet_precautions(file_id: int):
-#     db = SessionLocal()
-
-#     try:
-#         # Retrieve file data from the database
-#         excel_file = db.query(ExcelFile).filter(ExcelFile.id == file_id).first()
-#         if not excel_file:
-#             raise HTTPException(status_code=404, detail="File not found")
-
-#         # Parse Excel file data as bytes
-#         content_bytes = excel_file.file_data
-
-#         # Use BytesIO to create a file-like object
-#         excel_data = BytesIO(content_bytes)
-
-#         # Process the Excel data using pandas
-#         df = pd.read_excel(excel_data)
-
-#         # Process df and get text for OpenAI prompt
-#         prompt = df.to_string()
-#         # gpt_query = prompt + " from this given health data, give me recommendations"
-#         gpt_query = prompt + " from this given health data, give me recommendations on what food to eat and dietry precautions to take"
-
-#         client = OpenAI(api_key="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")
-
-#         def send_openai_request(gpt_query):
-#             response = client.chat.completions.create(
-#                 model="gpt-3.5-turbo-16k-0613",
-#                 messages=[{"role": "user", "content": gpt_query}],
-#                 temperature=0
-#             )
-#             return response.choices[0].message.content.strip()
-
-#         recommendations_text = send_openai_request(gpt_query)
-#         #print(recommendations_text)
-#         # Parse the OpenAI response to extract key-value pairs
-#         key_value_pairs = {}
-#         current_category = None
-
-#         lines = recommendations_text.split('\n')
-#         # print(lines)
-
-#         for line in recommendations_text.split('\n'):
-#             if line.startswith(str(current_category)):
-#                 current_category = line.split('.', 1)[1].strip()
-#             elif line.strip() and line[0].isdigit():
-#                 if '-' in line:
-#                     key, value = line.split('-', 1)
-#                     key = key.strip()
-#                     value = value.strip()
-#                     key_value_pairs[current_category] = key_value_pairs.get(current_category, []) + [(key, value)]
-
-#         # Convert the key-value pairs into a dictionary
-#         recommendations_dict = {"recommendations": key_value_pairs}
-
-
-
-#         return recommendations_dict
-
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
-
-#     finally:
-#         db.close()
-
-import json
 
 
 def parse_recommendations(recommendations_text):
